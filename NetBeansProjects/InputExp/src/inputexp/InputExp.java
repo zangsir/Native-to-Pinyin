5dd604db35ba85f0f600fbbee137a4b8b9a0ad7e
zangsir <zangsir@gmail.com>
2015-07-03 14:19:48 -0400

    added regex handling of texts in case of non-Chinese texts input

--- NetBeansProjects/InputExp/src/inputexp/InputExp.java
+++ NetBeansProjects/InputExp/src/inputexp/InputExp.java
@@ -7,8 +7,7 @@ package inputexp;
 
 /**
  *
- * @author Shuo Zhang
- * @ssz6@georgetown.edu
+ * @author zangsir
  */
 import java.util.Scanner;
 import java.util.Arrays;
@@ -22,53 +21,47 @@ public class InputExp {
    public static void main(String[] args) throws IOException  {
 	 
 		
-		//String input_path="/Users/zangsir/Desktop/inputfile.txt";
-                String input_path=args[0];
+		String input_path="/Users/zangsir/Desktop/lyrics.txt";
+                //String input_path=args[0];
 		ReadFile file = new ReadFile(input_path);
 		String[] aryLines = file.OpenFile();
 		//System.out.println(aryLines[0]);
+               //aryLines[i] is a line of sentence
+                for (String aryLine : aryLines) {
+                    //System.out.println(aryLines[i]);
+                    //ca[j] is a character or a space, catemp[k] is holding the temporary word for processing
+                    regexMatch checker = new regexMatch();
+                    char[] ca = aryLine.toCharArray();
+                    String[] pinyinOutput=new String[ca.length];
+                    //System.out.println(aryLines.length);
+                    for (int c=0; c<ca.length;c++){
 
-		//aryLines[i] is a line of sentence
-	for(int i=0; i<aryLines.length; i++){
-		//System.out.println(aryLines[i]);
-		//ca[j] is a character or a space, catemp[k] is holding the temporary word for processing
-		char[] ca=aryLines[i].toCharArray();
-	
-		String[] pinyinOutput=new String[ca.length];
+                        if (checker.checkAlpha(ca[c]) || !checker.checkRex(ca[c])){
+                            pinyinOutput[c]=Character.toString(ca[c]);
+                            continue;
+                        }
 
-		//System.out.println(aryLines.length);
-		for (int c=0; c<ca.length;c++){
-			  
-		    if (ca[c]=='，' || ca[c]=='。' || ca[c]==',' || ca[c]=='.' || ca[c]==' '){
-                        pinyinOutput[c]=Character.toString(ca[c]);
-                        continue;
-                    }
-                    
                         String[] pinyinArray = PinyinHelper.toHanyuPinyinStringArray(ca[c]);
-                    
-                    
-                    if(pinyinArray != null){
-                        pinyinOutput[c]=pinyinArray[0];
+
+
+                        if(pinyinArray != null){
+                            pinyinOutput[c]=pinyinArray[0];
+                        }
                     }
-                }
-		  
-                StringBuilder builder = new StringBuilder();
-		String finalOutput;
-
-		for(int y=0;y<pinyinOutput.length;y++){
-                    if(pinyinOutput[y]=="null"){		
-                        continue;
+                    StringBuilder builder = new StringBuilder();
+                    String finalOutput;
+                    for (String pinyinOutput1 : pinyinOutput) {
+                        if (pinyinOutput1 == "null") {
+                            continue;
+                        }
+                        builder.append(pinyinOutput1);
+                        builder.append(" ");
                     }
-                    builder.append(pinyinOutput[y]);
-                    builder.append(" ");
+                    finalOutput=builder.toString();
+                    System.out.println(finalOutput);
                 }
-		
-                finalOutput=builder.toString();
-		System.out.println(finalOutput);
-
-        }	  
-
-			   
+       
+       
    }    
 }
 		  
